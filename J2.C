97f753c0eb1f8b6da9253ffff777fa694dd18756
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-08-09 22:21:45 -0400

    Add files via upload

---
+++ J2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char name,no;
+	clrscr();
+
+	printf("enter your name ::");
+	scanf("%d",&name);
+
+	printf("enter your enrollment no ::");
+	scanf("%d",&no);
+
+	printf("\nyour name is::%d",name);
+	printf("your enrollment no is::%d",no);
+
+	getch();
+}
