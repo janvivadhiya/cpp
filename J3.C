8300fd83bb9858e2966222426267f1e44373cd6e
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-08-09 22:30:36 -0400

    Add files via upload

---
+++ J3.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,sum=0;
+	clrscr();
+
+	printf("enter number 1 ::");
+	scanf("%d",&n1);
+
+	printf("enter number 2 ::");
+	scanf("%d",&n2);
+
+	sum=n1+n2;
+
+	printf("sum ::%d",sum);
+	getch();
+}
+
+
